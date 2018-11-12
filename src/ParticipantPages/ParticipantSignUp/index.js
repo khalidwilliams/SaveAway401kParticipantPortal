187561c8dcc4c1e1b57a0edc0b60adc9908f0373
Khalid Williams <khalidwill27@gmail.com>
2018-11-11 20:53:59 -0700

    Create ParticipantSignUp component and modify logo styling for participant pages

---
+++ src/ParticipantPages/ParticipantSignUp/index.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class ParticipantSignUp extends Component {
+    <div className="participant-form">
+    
+    </div>
+}
--- src/ParticipantPages/index.js
+++ src/ParticipantPages/index.js
@@ -4,8 +4,12 @@ export default class ParticipantPages extends Component {
     
     render() {
         return (
-            <div>
-
+            <div id="participant-container">
+                <div className="participant-header">
+                    <img src={require("/Users/khalidwilliams/Projects/SaveAway401k/SaveAway401kParticipantPortal/src/ParticipantPages/Assets/ logo only.png")}></img>
+                    {/* Render the ParticipantSignUp section if no one is logged in (need to work with auth for this) */}
+                    
+                </div>
             </div>
         )
     }
